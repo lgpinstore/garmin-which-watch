d56ceacd99441fffc010077cd75b2edb210b9f7d
jhallginstore <jhall@lginstore.com>
2025-09-10 10:30:06 -0400

    adding a 5min timeout interval to return to home when inactive

--- public/assets/js/inactivity-timer.js
+++ public/assets/js/inactivity-timer.js
@@ -5,6 +5,7 @@
     // The duration of inactivity in milliseconds before redirecting.
     // Default is 5 minutes (5 * 60 * 1000).
     const TIMEOUT_DURATION = 5 * 60 * 1000; 
+    //const TIMEOUT_DURATION = 5000; 
 
     // The URL to redirect to after the timeout.
     const REDIRECT_URL = '/en-US/which-watch/start/index.html';
